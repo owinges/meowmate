70d74efe6ddda0383725f4a43fcf194e38f9310e
Owinges <owinges@gmail.com>
2018-07-22 01:05:25 +1000

    Implemented vector icons.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Icon size={30} name='ios-trash' color='red' />
         <Text>TEST!</Text>
       </View>
     );
